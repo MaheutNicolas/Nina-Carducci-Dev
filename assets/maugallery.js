0f3b5e8fded0648cb6042632837924bd996d469d
MaheutNicolas <maheutnicolas30@gmail.com>
2026-03-11 11:13:42 +0100

    feat: :sparkles: fix button galery ( active-tag -> active ) / add pointeur

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -183,7 +183,7 @@
           activeImage = $(this);
         }
       });
-      let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
+      let activeTag = $(".tags-bar span.active").data("images-toggle");
       let imagesCollection = [];
       if (activeTag === "all") {
         $(".item-column").each(function() {
@@ -228,7 +228,7 @@
           activeImage = $(this);
         }
       });
-      let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
+      let activeTag = $(".tags-bar span.active").data("images-toggle");
       let imagesCollection = [];
       if (activeTag === "all") {
         $(".item-column").each(function() {
@@ -297,7 +297,7 @@
      */
     showItemTags(gallery, position, tags) {
       var tagItems =
-        '<li class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></li>';
+        '<li class="nav-item"><span class="nav-link active active"  data-images-toggle="all">Tous</span></li>';
       $.each(tags, function(index, value) {
         tagItems += `<li class="nav-item active">
                 <span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
@@ -320,11 +320,11 @@
      * Gère également l'état actif visuel du bouton de filtre sélectionné.
      */
     filterByTag() {
-      if ($(this).hasClass("active-tag")) {
+      if ($(this).hasClass("active")) {
         return;
       }
-      $(".active-tag").removeClass("active active-tag");
-      $(this).addClass("active-tag");
+      $(".active").removeClass("active active");
+      $(this).addClass("active");
 
       var tag = $(this).data("images-toggle");
 
